54375f44ea1a62e2ee09282c33f100b2b27cd686
Jack <135305831+Sunny2049@users.noreply.github.com>
2025-01-07 11:17:28 +0800

    Deleting app/[locale]/(with-footer)/explore/page/[pageNum]/page.tsx

--- app/[locale]/(with-footer)/explore/page/[pageNum]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { RevalidateOneHour } from '@/lib/constants';
-
-import ExploreList from '../../ExploreList';
-
-export const revalidate = RevalidateOneHour * 6;
-
-export default function page({ params: { pageNum } }: { params: { pageNum: string | undefined } }) {
-  return <ExploreList pageNum={pageNum} />;
-}
